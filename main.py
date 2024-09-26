b2fcd0aba28dfcf0dfaa07fedcda3c535acb6c12
wertysed <wertysed11@gmail.com>
2024-09-26 19:12:38 +0300

    say my name

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello ")
+from fastapi import FastAPI, Body
+from typing import Annotated
+from pydantic import BaseModel
+
+
+
+class SayMyNameOut(BaseModel):
+    name: str
+
+class SayMyName(SayMyNameOut):
+    kilo_of_meth: int
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"hello": "Man"}
+
+@app.post("/say_my_name", response_model=SayMyNameOut)
+def say_my_name(user: Annotated[SayMyName, Body()]):
+    return user
